6c282544d9c6750a95b5be842b0e256281cfb85c
rodgers <rodgerstarrodgers@gmail.com>
2025-03-12 05:52:21 +0300

    home product api to fetch all products from db

--- app/api/products/list/route.js
+++ app/api/products/list/route.js
@@ -1,5 +1,3 @@
-import {getAuth} from "@clerk/nextjs/server";
-import authSeller from "@/lib/authSeller";
 import {NextResponse} from "next/server";
 import Product from "@/models/Product";
 import connectDB from "@/config/db";
@@ -7,14 +5,6 @@ import connectDB from "@/config/db";
 
 export async function GET(request) {
     try {
-
-        const { userId } = getAuth(request)
-
-        const isSeller = authSeller(userId)
-
-        if (!isSeller) {
-            return NextResponse.json({success: false, message: 'Not authorized'})
-        }
 
         await connectDB()
 
--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -27,7 +27,17 @@ export const AppContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({})
 
     const fetchProductData = async () => {
-        setProducts(productsDummyData)
+       try {
+           const {data} = await axios.get('api/products/list')
+
+           if (data.success){
+               setProducts(data.products)
+           }else {
+               toast.error(data.message)
+           }
+       }catch (error){
+           toast.error(error.message)
+       }
     }
 
     const fetchUserData = async () => {
